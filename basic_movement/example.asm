18e86e6c010b4da32517a0f93e83059271513fd1
Rick <menneric84@gmail.com>
2019-01-08 05:58:16 -0600

    grab our graphics instead

--- basic_movement/example.asm
+++ basic_movement/example.asm
@@ -283,13 +283,13 @@ palette:
 
 sprites:
      ;vert tile attr horiz
-  .byte $80, $32, $00, $80   ;sprite 0
-  .byte $80, $33, $00, $88   ;sprite 1
-  .byte $88, $34, $00, $80   ;sprite 2
-  .byte $88, $35, $00, $88   ;sprite 3
+  .byte $80, $05, $00, $80   ;sprite 0
+  .byte $80, $06, $00, $88   ;sprite 1
+  .byte $88, $15, $00, $80   ;sprite 2
+  .byte $88, $16, $00, $88   ;sprite 3
 
 	
 .segment "CHARS"
-    .incbin "mario.chr" ; include chr data file
+    .incbin "char.chr" ; include chr data file
 	
 	
